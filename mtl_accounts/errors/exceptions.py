9029c802c6c8dfd861ac9ae9d3591472f2916cc4
thdwoqor <celebarte01@gmail.com>
2022-05-11 18:28:59 +0900

    fix: remove unnecessary exceptions

--- mtl_accounts/errors/exceptions.py
+++ mtl_accounts/errors/exceptions.py
@@ -1,4 +1,3 @@
-from starlette.responses import JSONResponse
 
 
 class StatusCode:
@@ -12,57 +11,27 @@ class StatusCode:
 
 class APIException(Exception):
     status_code: int
-    code: str
-    msg: str
-    detail: str
+    message: str
     ex: Exception
 
     def __init__(
         self,
         *,
         status_code: int = StatusCode.HTTP_500,
-        code: str = "000000",
         msg: str = None,
-        detail: str = None,
         ex: Exception = None,
     ):
         self.status_code = status_code
-        self.code = code
         self.msg = msg
-        self.detail = detail
         self.ex = ex
         super().__init__(ex)
 
 
-class NotFoundAuthEx(APIException):
+class AccountExistsEx(APIException):
     def __init__(self, ex: Exception = None):
         super().__init__(
             status_code=StatusCode.HTTP_401,
-            msg=f"Authorization 요청 헤더가 없습니다.",
-            detail=f"Authorization is empty.",
-            code=f"{StatusCode.HTTP_401}{'2001'}",
-            ex=ex,
-        )
-
-
-class TokenInvalidEx(APIException):
-    def __init__(self, ex: Exception = None):
-        super().__init__(
-            status_code=StatusCode.HTTP_401,
-            msg=f"토큰값이 유효하지 않아 인증에 실패했습니다.",
-            detail="Token is invalid.",
-            code=f"{StatusCode.HTTP_401}{'2003'}",
-            ex=ex,
-        )
-
-
-class TokenDecodeEx(APIException):
-    def __init__(self, ex: Exception = None):
-        super().__init__(
-            status_code=StatusCode.HTTP_401,
-            msg=f"Authorization 요청 헤더의 토큰 타입이 유효하지 않습니다.",
-            detail="Token type is invalid.",
-            code=f"{StatusCode.HTTP_401}{'2002'}",
+            message=f"Account is exists",
             ex=ex,
         )
 
@@ -71,19 +40,6 @@ class AuthExpiredEx(APIException):
     def __init__(self, ex: Exception = None):
         super().__init__(
             status_code=StatusCode.HTTP_401,
-            msg=f"인증이 만료되었습니다.",
-            detail=f"Authentication is expired",
-            code=f"{StatusCode.HTTP_401}{'3001'}",
-            ex=ex,
-        )
-
-
-class AccountExistsEx(APIException):
-    def __init__(self, ex: Exception = None):
-        super().__init__(
-            status_code=StatusCode.HTTP_401,
-            msg=f"계정이 이미 존재합니다.",
-            detail=f"Account is exists",
-            code=f"{StatusCode.HTTP_401}{'3002'}",
+            msg=f"Authentication is expired",
             ex=ex,
         )
