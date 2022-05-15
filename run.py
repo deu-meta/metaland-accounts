d725ebea41ed518731f24af5dc28580a3d1c6eb4
solo5star <solo_5@naver.com>
2022-05-15 19:10:15 +0900

    fix: fix to proper entrypoint

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 import uvicorn
 from dotenv import load_dotenv
 
-from mtl_accounts.main import app
-
 load_dotenv(verbose=True)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("mtl_accounts.main:app", host="0.0.0.0", port=8000, reload=True)
