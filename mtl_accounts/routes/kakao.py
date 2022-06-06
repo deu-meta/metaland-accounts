a297fdb8ce84250013e089aaec8d6578cef47915
solo5star <solo_5@naver.com>
2022-06-07 06:45:56 +0900

    fix: fix error when pass datetime into `Authorization.create_access_token`

--- mtl_accounts/routes/kakao.py
+++ mtl_accounts/routes/kakao.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from fastapi import APIRouter, Depends, Request
@@ -40,10 +41,10 @@ async def kakao_callback(request: Request, Authorize: AuthJWT = Depends(), sessi
 
     account = create_or_update_user(session, user)
 
-    access_token = Authorize.create_access_token(subject=account.id, user_claims=account.dict())
+    access_token = Authorize.create_access_token(subject=account.id, user_claims=json.loads(account.json()))
     response = RedirectResponse(f"{JWT_REDIRECT_URL}#access_token={access_token}")
 
-    refresh_token = Authorize.create_refresh_token(subject=account.id, user_claims=account.dict())
+    refresh_token = Authorize.create_refresh_token(subject=account.id, user_claims=json.loads(account.json()))
 
     Authorize.set_refresh_cookies(refresh_token, response, max_age=1209600)
 
