588c2f08b5717f86164515bea8f78a1ddbfb01fc
solo5star <solo_5@naver.com>
2022-06-08 20:49:05 +0900

    refactor: remove hard-coded value and refine the sentence

--- mtl_accounts/util/sso/kakao.py
+++ mtl_accounts/util/sso/kakao.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from fastapi_sso.sso.base import SSOBase
 from mtl_accounts.models import OpenID
 
@@ -26,5 +26,6 @@ class KakaoSSO(SSOBase):
     @classmethod
     async def openid_from_response(cls, response: dict) -> OpenID:
         if dict(response["kakao_account"]).get("email") is None:
-            raise HTTPException(status_code=404, detail="Please check your email consent")
+            raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY, "이메일 제공 동의 여부를 확인해주세요.")
+
         return OpenID(display_name=response["properties"]["nickname"], email=response["kakao_account"]["email"], provider=cls.provider)
